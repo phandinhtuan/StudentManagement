2ba93a6134cdf0079c9364e98373ba30f955fa14
tuanpd <tuanpd.sc@gmail.com>
2020-05-19 23:00:44 +0700

    add

---
+++ StudentManagement/DAL/ListUserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagement.DAL
+{
+    public class ListUserRole
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string RoleId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ StudentManagement/Models/usp_AspNetUserRoles_GetList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagement.Models
+{
+    
+    public class usp_AspNetUserRoles_GetList
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Name { get; set; }
+
+        public string RoleId { get; set; }
+    }
+
+}
