a99449f76b6f056c139bb4a0b3a52c4864a862e0
tuanpd <tuanpd.sc@gmail.com>
2020-05-25 11:46:53 +0700

    add Models/usp_AspNetUser_GetList.cs

---
+++ StudentManagement/Models/usp_AspNetUser_GetList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagement.Models
+{
+    public class usp_AspNetUser_GetList
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+    }
+
+}
