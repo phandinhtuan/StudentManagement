76e6815a57d3297fb5dbd08f3fd1a62ae16bf6e3
tuanpd <tuanpd.sc@gmail.com>
2020-06-01 09:19:55 +0700

    upate file

---
+++ StudentManagement/DAL/Service/AspNetUserService.cs
@@ -0,0 +1,26 @@
+﻿using StudentManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagement.DAL.Service
+{
+    public class AspNetUserService: GenericService<AspNetUser>
+    {
+        public List<usp_AspNetUser_GetList> GetListUser()
+        {
+            var lst = new List<usp_AspNetUser_GetList>();
+            try
+            {
+                string strSql = "EXEC usp_AspNetUser_GetList";
+                lst = _db.Database.SqlQuery<usp_AspNetUser_GetList>(strSql).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return lst;
+        }
+    }
+}
